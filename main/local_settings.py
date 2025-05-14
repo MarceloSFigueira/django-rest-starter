ee3036ecf46d8c627e602e00fe7771ba4a506521
MarceloSFigueira <jsaraivafigueira@gmail.com>
2025-05-14 17:45:44 -0300

    edit CORS

--- main/local_settings.py
+++ main/local_settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",              
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -72,16 +72,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "main.wsgi.application"
 
 CORS_ALLOW_CREDENTIALS = True 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000', 
-    'http://localhost:8080',
-    'https://orcamento-comercial-production-ee0c.up.railway.app'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "https://orcamento-comercial-production-ee0c.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',  
-    'http://localhost:8080',  
-    'https://orcamento-comercial-production-ee0c.up.railway.app'
+    "https://orcamento-comercial-production-ee0c.up.railway.app",
+    "http://localhost:3000",
+    "http://localhost:8080",
 ]
 
 CSRF_COOKIE_SAMESITE = 'Lax'
